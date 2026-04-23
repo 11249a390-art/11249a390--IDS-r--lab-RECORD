99d45d141c0cd10b65dff825a7476fa9f30c1ea6
11249a390-art <11249a390@kanchiuniv.ac.in>
2026-04-23 20:00:37 +0530

    1.Basics of R- Programming.r

---
+++ 1.Basics of R- Programming.r
@@ -0,0 +1,112 @@
+EXERCISE 01 - BASICS OF R - PROGRAMMING
+
+1.1:
+Student Information System
+
+R Program:
+roll_no <- 390 # Integer
+name <- "UJWALA" # Character
+cgpa <- 9.4 # Numeric
+pass <- TRUE # Logical
+roll_no
+name
+cgpa
+pass
+
+
+1.2:
+Employee Salary Record
+
+R Program:
+emp_id <- 9001
+salary <- 55000.75
+department <- "CSE"
+is_permanent <- TRUE
+emp_id
+salary
+department
+is_permanent
+
+
+1.3:
+Online Shopping Cart
+
+R Program:
+product_name <- "I-PHONE"
+price <- 100000
+stock_available <- 10
+discount_eligible <- FALSE
+product_name
+price
+stock_available
+discount_eligible
+
+
+1.4:
+Weather Monitoring System
+
+R Program:
+city <- "KADAPA"
+temperature <- 35
+rain <- FALSE
+wind_speed <- 16.2
+city
+temperature
+rain
+wind_speed
+
+
+1.5:
+Examination Result Processing
+
+R Program:
+subject <- "R-LAB"
+total_marks <- 100
+marks_obtained <- 95
+result <- marks_obtained >= 40
+subject
+total_marks
+marks_obtained
+result
+
+
+1.6:
+Banking Application
+
+R Program:
+account_no <- 12345A567
+holder_name <- "Bhanu Prakash"
+balance <- 950000.50
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+
+1.7:
+Movie Rating System
+
+R Program:
+movie_title <- "18-Pages"
+rating <- 10.10
+reviews <- 19000
+available <- TRUE
+movie_title
+rating
+reviews
+available
+
+
+1.8:
+Traffic Monitoring System
+
+R Program:
+vehicle_count <- 590
+location <- "Highway Road"
+avg_speed <- 60
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
